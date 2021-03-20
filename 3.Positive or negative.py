2822446f0bb719cbe6b5c0a91296e524ebe8d748
GT007-tech <80970496+GT007-tech@users.noreply.github.com>
2021-03-20 21:38:22 +0530

    Add files via upload

---
+++ 3.Positive or negative.py
@@ -0,0 +1,7 @@
+x=float(input("Enter the number"))
+if x>0:
+  print('the number entered is positive')
+elif x<0:
+  print('the number entered is negative')
+else:
+  print('the number entered is equal to zero')
